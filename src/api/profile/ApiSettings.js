fa62444011a5dfdc677fff618dea9d80e47949e1
Naimeless <65905724+Naimeless@users.noreply.github.com>
2022-05-11 14:51:33 +0300

    запросы

--- src/api/profile/ApiSettings.js
+++ src/api/profile/ApiSettings.js
@@ -1,5 +1,5 @@
 async function ApiSettings(){
-    let response = await fetch ('https://virtserver.swaggerhub.com/math-god/Sixgram.Auth/1.0/api/v1/user/changeavatar')
+    let response = await fetch ('http://192.168.0.122:85/api/v1/user/changeavatar')
     let content = await response.json() 
     
     console.log(content)
